012a0e856075d05abd52947844586f9bdf8430be
myousuf65 <official.yousufpathan@gmail.com>
2026-06-10 21:11:55 +0800

    admin auth + guest browsable by name

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module yousuf.xyz/file-server-with-auth
 
 go 1.26.2
+
+require (
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
+
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
 )
+
+
+func signToken(username string, secret string) string {
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write([]byte(username))
+	return hex.EncodeToString(mac.Sum(nil))
+}
+
+func checkSignToken(receivedToken string, storedToken string) bool {
+	if receivedToken == storedToken {
+		return true
+	}
+	return false
+}
 
 func main() {
 	port := ":8080"
-	dir := "/data/" 
+	dir := "/Users/myousuf/dev/obs-notes/obsdian-notes/dist/"
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file: %v", err)
+	}
+
+	var secret = os.Getenv("secret")
+	var store = sessions.NewCookieStore([]byte(secret))
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := filepath.Join(dir, filepath.Clean(r.URL.Path))
-		
+
 		// Get file info
 		info, err := os.Stat(path)
 		if err != nil {
@@ -23,18 +55,100 @@ func main() {
 			}
 			return
 		}
-		
-		// If it's a directory, return 404
+
 		if info.IsDir() {
 			http.NotFound(w, r)
 			return
 		}
-		
-		// Serve the file
+
 		http.ServeFile(w, r, path)
 	})
 
+	mux.HandleFunc("GET /login", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "admin.html")
+	})
+
+	mux.HandleFunc("POST /login", func(w http.ResponseWriter, r *http.Request) {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		if username == os.Getenv("username") && password == os.Getenv("password") {
+			secret_key := os.Getenv("secret")
+			token := signToken(username, secret_key)
+
+			tokenCookie := &http.Cookie{
+				Name:     "session_token",
+				Value:    token,
+				Path:     "/admin",
+				Expires:  time.Now().Add(24 * time.Hour),
+				HttpOnly: true,
+				Secure:   true,
+				SameSite: http.SameSiteLaxMode,
+			}
+
+			usernameCookie := &http.Cookie{
+				Name:     "username",
+				Value:    username,
+				Path:     "/admin",
+				Expires:  time.Now().Add(24 * time.Hour),
+				HttpOnly: true,
+				Secure:   true,
+				SameSite: http.SameSiteLaxMode,
+			}
+
+			session, _ := store.Get(r, "user-session")
+			session.Values[username] = token
+			err := session.Save(r, w)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			http.SetCookie(w, tokenCookie)
+			http.SetCookie(w, usernameCookie)
+			http.Redirect(w, r, "/admin", 302)
+			return
+		}
+
+	})
+
+	fileDir := http.Dir(dir)
+	fileServer := http.StripPrefix("/admin/", http.FileServer(fileDir))
+
+	mux.Handle("/admin/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		received_token, err := r.Cookie("session_token")
+		if err != nil {
+			//TODO return 
+			fmt.Println("Error getting token from cookie")
+			return
+		}
+		username, err := r.Cookie("username")
+		if err != nil {
+			//TODO return
+			fmt.Println("Error getting username from cookie")
+			return
+		}
+
+		session, _ := store.Get(r, "user-session")
+		token, ok := session.Values[username.Value].(string)
+		if !ok {
+			http.Error(w, "Unauthorized: No session found", http.StatusUnauthorized)
+			return
+		}
+
+		if checkSignToken(received_token.Value, token) {
+			fileServer.ServeHTTP(w, r)
+		}
+	
+		//TODO graceful return
+		return
+	}))
+
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		fmt.Println("Error starting server")
+	}
 	println("Server running at http://localhost" + port)
 	println("Directories will NOT be served (404)")
-	http.ListenAndServe(port, nil)
 }
